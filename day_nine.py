786a3889dcee6e17e1a80f1ada3a58268653ac95
Hassain Saeed <hassain.saeed@gmail.com>
2023-01-20 01:23:58 -0800

    Completed AoC 2022 Day 9 pt 2

--- day_nine.py
+++ day_nine.py
@@ -63,6 +63,79 @@ def solve():
 
     return len(unique_tail_pos)
 
+def solve_part_two():
+    input_object = input_file_reader.FileReader('input_files/input_day9.txt')
+    motions_list = input_object.read_input_file()
+
+    rope = [[0,0] for _ in range(10)]
+
+    og_pos = "0,0"
+    unique_tail_pos = set()
+    unique_tail_pos.add(og_pos)
+
+    for motion in motions_list:
+        if (motion == ""):
+            continue
+        direction = motion.split(" ")[0]
+        distance = int(motion.split(" ")[1])
+
+        for i in range(distance):
+            if direction == "R":
+                rope[0][1] += 1
+            elif direction == "L":
+                rope[0][1] -= 1
+            elif direction == "U":
+                rope[0][0] += 1
+            elif direction == "D":
+                rope[0][0] -= 1
+            
+            # print(direction)
+            # print(rope)
+
+            for i in range(1,10):
+                # check of head and tail are one apart. If so do nothing
+                if ((abs(rope[i-1][1] - rope[i][1]) == 1) or (abs(rope[i-1][1] - rope[i][1]) == 0)) and ((abs(rope[i-1][0] - rope[i][0]) == 1) or (abs(rope[i-1][0] - rope[i][0]) == 0)):
+                    # print("A i: " + str(i))
+                    # print(rope[i])
+                    continue;
+                else:
+                    # If head and tail not one apart, move tail towards head
+                    # if they are on the same y axis, move tail to head
+                    if (rope[i-1][0] == rope[i][0]):
+                        if (rope[i-1][1] == rope[i][1] + 2):
+                            rope[i][1] += 1
+                        elif (rope[i-1][1] == rope[i][1] - 2):
+                            rope[i][1] -= 1
+                    # if they are in the same x axis, move tail up/down to head 
+                    elif (rope[i-1][1] == rope[i][1]):
+                        if (rope[i-1][0] == rope[i][0] + 2):
+                            rope[i][0] += 1
+                        elif (rope[i-1][0] == rope[i][0] - 2):
+                            rope[i][0] -= 1
+                    elif (rope[i-1][1] > rope[i][1] and rope[i-1][0] < rope[i][0]):
+                        rope[i][1] += 1
+                        rope[i][0] -= 1
+                    elif (rope[i-1][1] > rope[i][1] and rope[i-1][0] > rope[i][0]):
+                        rope[i][1] += 1
+                        rope[i][0] += 1
+                    elif (rope[i-1][1] < rope[i][1] and rope[i-1][0] > rope[i][0]):
+                        rope[i][1] -= 1
+                        rope[i][0] += 1
+                    elif (rope[i-1][1] < rope[i][1] and rope[i-1][0] < rope[i][0]):
+                        rope[i][1] -= 1
+                        rope[i][0] -= 1
+                    # print("B i: " + str(i))
+                    # print(rope[i])
+            
+            # print(rope[9])
+            unique_tail_pos.add(str(rope[9][0]) + ',' + str(rope[9][1]))
+        
+    return len(unique_tail_pos)
+
+
+
 answer = solve()
 print(answer)
+answer_part_two = solve_part_two()
+print(answer_part_two)
 
